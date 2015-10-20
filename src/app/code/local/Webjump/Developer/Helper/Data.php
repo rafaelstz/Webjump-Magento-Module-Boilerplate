9bb3ba9fb1b67ddd1493c30cfd8488eae7520857
Rafael Corrêa Gomes <rafaelcg_stz@hotmail.com>
2015-10-20 14:15:09 -0200

    Remove method breadcumbs

--- src/app/code/local/Webjump/Developer/Helper/Data.php
+++ src/app/code/local/Webjump/Developer/Helper/Data.php
@@ -2,19 +2,4 @@
 
 class Webjump_Developer_Helper_Data extends Mage_Core_Helper_Abstract {
 
-	public function getBreadcrumbs($namePage){
-		$html = null;
-		$sepateElement = "&gt";
-		if($namePage):
-			$html = '<div class="breadcrumbs">
-					    <ul>
-					          <li class="home">
-					            <a href="'.Mage::getBaseUrl().'" title="'.$this->__("Go to Home Page").'">'.$this->__("Home").'</a><span>'.$sepateElement.'</span>
-					          </li>
-					          <li class="page"><strong>'.$this->__($namePage).'</strong></li>
-					     </ul>
-					</div>';
-		endif;
-		return $html;
-	}
 }
